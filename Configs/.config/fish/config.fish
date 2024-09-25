3bd34a48ffe405fbdbbd162e9ba9c4b6a5d4ea9a
trungkien71297 <trungkien71297@gmail.com>
2024-09-25 08:42:42 +0700

    Self config

--- Configs/.config/fish/config.fish
+++ Configs/.config/fish/config.fish
@@ -4,19 +4,8 @@ if status is-interactive
     starship init fish | source
 end
 
-# List Directory
-alias l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
-alias lt='eza --icons=auto --tree' # list folder as tree
+source ~/.peemoti_config/alias
 
-# Handy change dir shortcuts
-abbr .. 'cd ..'
-abbr ... 'cd ../..'
-abbr .3 'cd ../../..'
-abbr .4 'cd ../../../..'
-abbr .5 'cd ../../../../..'
-
-# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
-abbr mkdir 'mkdir -p'
+function fish_greeting
+  pokeget random | fastfetch --file-raw -
+end
